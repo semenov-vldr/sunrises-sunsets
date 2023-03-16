8c1f55a4c416018658b79fddf2f799747a18d041
semenov-vldr <semenov-vldr@users.noreply.github.com>
2023-03-16 16:14:10 +0000

    deploy: bad3bb776385d279c4be614445cdc85a237316e3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,31 @@
 
+const API_KEY = '&appid=fd04c038b4083dd0d159274298038549';
+const API_URL = 'https://api.openweathermap.org/data/2.5/weather?id=';
+const CITY_ID = 697530;
+
+
+const headerWeather = document.querySelector('.header .header__weather');
+
+if (headerWeather) {
+
+  // Проверка знака температуры
+  const checkPolarity = (value) => value <= 0 ? '' + value: '+' + value;
+
+  const tempWater = headerWeather.querySelector('.header__weather-item-value--water');
+  const tempAir = headerWeather.querySelector('.header__weather-item-value--clouds');
+
+
+  fetch(API_URL + CITY_ID + API_KEY)
+    .then((responce) => responce.json())
+    .then(res => {
+      console.log(res);
+      //const waterValue = `${checkPolarity(Math.round(res.main.temp - 273))}°C`;
+      const airValue = `${checkPolarity(Math.round(res.main.temp - 273))}°C`;
+      //tempWater.textContent = waterValue
+      tempAir.textContent = airValue
+    })
+
+
+
+}
+
