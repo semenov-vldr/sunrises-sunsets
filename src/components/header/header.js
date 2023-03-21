b5089427aa459dab25f266a0eabf4a4e7eff54d2
Vladimir <vova2066@yandex.ru>
2023-03-21 17:25:33 +0300

    ref footer

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,28 +3,70 @@ const API_URL = 'https://api.openweathermap.org/data/2.5/weather?id=';
 const CITY_ID = 697530;
 
 
+const header = document.querySelector('.header');
+
 const headerWeather = document.querySelector('.header .header__weather');
 
-if (headerWeather) {
-
-  // Проверка знака температуры
-  const checkPolarity = (value) => value <= 0 ? '' + value: '+' + value;
-
-  const tempWater = headerWeather.querySelector('.header__weather-item-value--water');
-  const tempAir = headerWeather.querySelector('.header__weather-item-value--clouds');
+if (headerWeather) fetchWeatherValues();
 
 
-  fetch(API_URL + CITY_ID + API_KEY)
-    .then((responce) => responce.json())
-    .then(res => {
-      console.log(res);
-      //const waterValue = `${checkPolarity(Math.round(res.main.temp - 273))}°C`;
-      const airValue = `${checkPolarity(Math.round(res.main.temp - 273))}°C`;
-      //tempWater.textContent = waterValue
-      tempAir.textContent = airValue
-    })
+  function fetchWeatherValues () {
+    // Проверка знака температуры
+    const checkPolarity = (value) => value <= 0 ? '' + value: '+' + value;
 
+    const tempWater = headerWeather.querySelector('.header__weather-item-value--water');
+    const tempAir = headerWeather.querySelector('.header__weather-item-value--clouds');
+
+    fetch(API_URL + CITY_ID + API_KEY)
+      .then((responce) => responce.json())
+      .then(res => {
+        console.log(res);
+        //const waterValue = `${checkPolarity(Math.round(res.main.temp - 273))}°C`;
+        const airValue = `${checkPolarity(Math.round(res.main.temp - 273))}°C`;
+        //tempWater.textContent = waterValue
+        tempAir.textContent = airValue
+      })
+  };
+
+
+if (header) {
+
+  let previousPosition = document.documentElement.scrollTop;
+  function scrollHeader (header) {
+    let currentPosition = document.documentElement.scrollTop;
+
+    if (previousPosition > currentPosition || window.scrollY < 100) {
+      header.classList.remove('js-scroll');
+      header.classList.add('js-scroll-bg');
+
+    } else {
+      header.classList.add('js-scroll');
+    }
+    if (previousPosition === currentPosition || window.scrollY === 0) {
+      header.classList.remove('js-scroll-bg');
+    }
+    previousPosition = currentPosition;
+  }
+  // scroll
+  window.addEventListener('scroll', () => scrollHeader (header) );
+
+
+
+  // const navLinksSubNav = header.querySelectorAll('.header__nav-link.arrow-link, .header__tel');
+  //
+  // navLinksSubNav.forEach(link => {
+  //   link.addEventListener('mouseover', () => {
+  //     header.classList.add('hover-link');
+  //   })
+  //
+  //   link.addEventListener('mouseout', () => {
+  //     header.classList.remove('hover-link');
+  //   })
+  // })
 
 
 }
+
+
+
 
