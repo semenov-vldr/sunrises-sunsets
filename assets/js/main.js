c0e0a69aece9be5f552a632963e1d36263c2fbb1
semenov-vldr <semenov-vldr@users.noreply.github.com>
2023-03-24 15:12:21 +0000

    deploy: dec3791265418a3acbc6315b1d8e90002e9389b5

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,7 +65,7 @@ if (header) {
     // Mobile
     if (widthMobile.matches) {
 
-      elem.addEventListener('click', (evt) => {
+      elem.addEventListener('click', () => {
 
           SubNav.classList.toggle('js-menu-active');
 
@@ -79,7 +79,7 @@ if (header) {
                 SubNav.classList.remove('js-menu-active');
                 unblockScrollPage();
               }
-            })
+            });
 
 
           } else {
@@ -151,18 +151,38 @@ if (header) {
 const html = document.documentElement;
 
 
+let div = document.createElement('div');
+
+div.style.overflowY = 'scroll';
+div.style.width = '50px';
+div.style.height = '50px';
+
+document.body.append(div);
+let scrollWidth = div.offsetWidth - div.clientWidth;
+div.remove();
+
+
+
+
 function blockScrollPage () {
   if ( !html.classList.contains('js-block-scroll') ) {
     html.classList.add('js-block-scroll');
+    html.style.marginRight = `${scrollWidth}px`
   }
 };
 
 function unblockScrollPage () {
   if ( html.classList.contains('js-block-scroll') ) {
     html.classList.remove('js-block-scroll');
+
+    html.style.marginRight = '';
   }
 };
 
 function toggleScrollPage () {
   html.classList.toggle('js-block-scroll');
 };
+
+
+
+
